412f995479ab62da1d44c3b449041bb604445f85
anast3t <bortnikov20000@mail.ri>
2022-10-17 09:09:40 +0700

    print/printOld

--- task10/src/main/kotlin/Printer.kt
+++ task10/src/main/kotlin/Printer.kt
@@ -1,6 +1,22 @@
 class Printer {
     private val synchronizer = Object()
     private var safetyFlag:Boolean = false
+
+    private fun notificationPart(loopStep: Int, name: String, safetyFlagStep: Boolean){
+        println("$loopStep--$name changING $safetyFlag -> $safetyFlagStep")
+        safetyFlag = safetyFlagStep
+        println("$loopStep--$name changED. Curr: $safetyFlag")
+        synchronizer.notifyAll()
+        println("$loopStep--$name called notifyAll()")
+    }
+
+    private fun sleepPart(loopStep: Int, name: String, safetyFlagStep: Boolean){
+        if(safetyFlag == safetyFlagStep){
+            println("$loopStep--$name going wait: $safetyFlag === $safetyFlagStep")
+            while (safetyFlag == safetyFlagStep)
+                synchronizer.wait()
+        } else println("$loopStep--$name-s step, not waiting: $safetyFlag !== $safetyFlagStep")
+    }
     fun print(name: String, msg: String, safetyFlagStep: Boolean, loopStep: Int){
         /*Здесь получается неприятное для задачи поведение -
           треды почти никогда не уходят в сон, по факту работая на синхронайзере
@@ -19,7 +35,7 @@ class Printer {
     fun printOld(name: String, msg: String, safetyFlagStep: Boolean, loopStep: Int, last: Boolean){
 
         /*А вот здесь как изначально и задумывалось, по очереди просыпаются и засыпают*/
-        
+
         println("$loopStep--$name Entered part BEFORE synchronizer")
         synchronized(synchronizer){
             println("$loopStep--$name Entered part AFTER synchronizer")
@@ -33,19 +49,5 @@ class Printer {
         }
     }
     
-    private fun notificationPart(loopStep: Int, name: String, safetyFlagStep: Boolean){
-        println("$loopStep--$name changING $safetyFlag -> $safetyFlagStep")
-        safetyFlag = safetyFlagStep
-        println("$loopStep--$name changED. Curr: $safetyFlag")
-        synchronizer.notifyAll()
-        println("$loopStep--$name called notifyAll()")
-    }
 
-    private fun sleepPart(loopStep: Int, name: String, safetyFlagStep: Boolean){
-        if(safetyFlag == safetyFlagStep){
-            println("$loopStep--$name going wait: $safetyFlag === $safetyFlagStep")
-            while (safetyFlag == safetyFlagStep)
-                synchronizer.wait()
-        } else println("$loopStep--$name-s step, not waiting: $safetyFlag !== $safetyFlagStep")
-    }
 }
