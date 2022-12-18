1b924cd23fc78169a3c257e79fdf584b3d4775c0
anast3t <bortnikov20000@mail.ri>
2022-12-19 02:07:27 +0700

    task2+tests

--- clojure/task2/src/task2/core.clj
+++ clojure/task2/src/task2/core.clj
@@ -1,6 +1,14 @@
 (ns task2.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn sieve-of-eratosthenes
+  [n]
+  (let [numbers (range 2 n) primes []] ;
+    (loop [numbers numbers primes primes]
+      (if (empty? numbers)
+        primes
+        (let [prime (first numbers)]
+          (recur (remove #(zero? (mod % prime)) numbers) (conj primes prime)))))))
+
+
+;(print (remove #(zero? (mod % 2)) [1, 2, 3, 4, 5]))
+(println (sieve-of-eratosthenes 0))
--- clojure/task2/test/task2/core_test.clj
+++ clojure/task2/test/task2/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [task2.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest basic
+  (testing "Basic test"
+    (is (= [2, 3] (sieve-of-eratosthenes 4)))))
+
+(deftest zero
+  (testing "Zero test"
+    (is (= [] (sieve-of-eratosthenes 0)))))
+
+(deftest firstTen
+  (testing "First ten prime numbers"
+    (is (= [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] (sieve-of-eratosthenes 30)))))
