4cbe252e160ff555e618503471f5fa878aff8d76
anast3t <bortnikov20000@mail.ri>
2022-12-28 21:59:33 +0700

    clj 2 full

--- clojure/task2/src/task2/core.clj
+++ clojure/task2/src/task2/core.clj
@@ -1,37 +1,25 @@
 (ns task2.core)
-
-(defn sieve-of-eratosthenes
-  [n]
-  (let [numbers (range 2 n) primes []]
-    (loop [numbers numbers primes primes]
-      (if (empty? numbers)
-        primes
-        (let [prime (first numbers)]
-          (recur (remove #(zero? (mod % prime)) numbers) (conj primes prime)))))))
-
-
-;(print (remove #(zero? (mod % 2)) [1, 2, 3, 4, 5]))
-;(println (sieve-of-eratosthenes 0))
 (defn new-filter-layer
-  [number s]
-  (filter
-    (fn [t]
-      (not (= (mod t number) 0)))
-    s))
+  [number s] (filter
+               (fn [t]
+                 (not (= (mod t number) 0)))
+               s))
 (defn prime-set
   ([]
    (let [first-prime 2]
-     (lazy-seq
-       (cons first-prime
-             (new-filter-layer first-prime (iterate inc 2))))))
-
+     (lazy-seq (cons first-prime
+                     (prime-set (new-filter-layer first-prime (iterate inc 2)))))))
   ([s]
-   (let [prime-now (take 1 s)]
+   (let [prime-now (nth (take 1 s) 0)]
      (lazy-seq
        (cons
          prime-now
          (prime-set
-           (new-filter-layer prime-now s)))))))
-(println (take 10
-               (prime-set)))
+           (new-filter-layer prime-now s))))
+     )))
+
+
+;(print (doall (take 20 (prime-set))))
+
+
 
--- clojure/task2/test/task2/core_test.clj
+++ clojure/task2/test/task2/core_test.clj
@@ -1,15 +1,15 @@
 (ns task2.core-test
   (:require [clojure.test :refer :all]
             [task2.core :refer :all]))
-
+;
 (deftest basic
   (testing "Basic test"
-    (is (= [2, 3] (sieve-of-eratosthenes 4)))))
+    (is (= [2, 3] (take 2 (prime-set))))))
 
 (deftest zero
   (testing "Zero test"
-    (is (= [] (sieve-of-eratosthenes 0)))))
+    (is (= [] (take 0 (prime-set))))))
 
 (deftest firstTen
   (testing "First ten prime numbers"
-    (is (= [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] (sieve-of-eratosthenes 30)))))
+    (is (= (vec (take 10 (prime-set))) (vec [2, 3, 5, 7, 11, 13, 17, 19, 23, 29])))))
